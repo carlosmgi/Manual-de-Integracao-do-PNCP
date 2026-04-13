cfa960b4f4f256d9eee565ac14de717936a7c2cf
carlosmgi <carlos-eduardo.melo@gestao.gov.br>
2026-04-13 16:24:05 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Permite destacar as linhas das tabelas via .CSS
-
 from docutils import nodes
 from docutils.parsers.rst import roles
 
@@ -11,9 +10,7 @@ def destaque_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
 
 roles.register_local_role('destaque', destaque_role)
 
-
 #-- Alterar a versão do manual em todas as páginas
-
 version = "2.5"
 
 rst_epilog = f"""
@@ -21,7 +18,6 @@ rst_epilog = f"""
 """
 
 # -- Project information
-
 project = 'Manual de Integração do PNCP'
 copyright = '2026, Ministério da Gestão e Inovação em Serviços Públicos - MGI'
 author = 'COPNCP/CGGES/DELOG/SEGES/MGI'
@@ -30,7 +26,6 @@ release = '2.3.10'
 version = '2.3.10'
 
 # -- General configuration
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -48,14 +43,17 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Permite destacar as linhas "kbd" das tabelas via .CSS
-
 html_static_path = ['_static']
-
 html_css_files = ['custom.css',]
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Options for PDF output
+latex_documents = [
+    ('index', 'project.tex', 'Project Documentation',
+     'Author', 'manual'),
+]
