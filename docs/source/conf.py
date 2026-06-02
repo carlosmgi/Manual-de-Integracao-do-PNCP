5d797bad6321d3e9f47577f790ba27dad658019c
carlosmgi <carlos-eduardo.melo@gestao.gov.br>
2026-06-02 17:25:21 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,3 +77,9 @@ latex_documents = [
     ('index', 'project.tex', 'Project Documentation',
      'Author', 'manual'),
 ]
+
+# -- Ativa o modal de pesquisa do Read the Docs ao focar no input de busca do seu tema
+document.querySelector("[role='search'] input").addEventListener("focusin", () => {
+    const event = new CustomEvent("readthedocs-search-show");
+    document.dispatchEvent(event);
+});
